8b1f3dc5e2b73f407a291ac06deaa6148b5ed567
Harisha S <harishaa.sa@gmail.com>
2023-09-08 11:36:51 +0530

    Added Redirect Page

--- background.js
+++ background.js
@@ -8,14 +8,16 @@ function IsBlockedSite(tab) {
         if (data.BlockedUrl !== undefined) {
             if (data.BlockedUrl.includes(hostname)) {
                 console.log("Dangerous tab. Close it for inner peace");
-                chrome.tabs.discard(tab.id);
+                //chrome.tabs.discard(tab.id);
                 //chrome.tabs.remove(tab.id, ()=>chrome.runtime.lastError)
-                //ReDirectPage(tab);
+                ReDirectPage(tab);
             }
         }
     })
 }
 
 function ReDirectPage(tab) {
-    chrome.tabs.update(tab.id, { url: "https://www.google.com" });
+    chrome.tabs.update(tab.id, {
+        url: chrome.runtime.getURL("./pages/areyoulost.html")});
 }
+
